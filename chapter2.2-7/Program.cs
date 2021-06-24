a08bd9f6e07fda008c4a8fba015ffd3b2f0ae066
username <1151555861@qq.com>
2021-06-24 13:50:32 +0800

    first commit

---
+++ chapter2.2-7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chapter2._2_7
+{
+    class Program
+    {
+        static void PrintType<T>()
+        {
+            Console.WriteLine("typef(T) = {0}", typeof(T));
+            Console.WriteLine("typef(List<T>) = {0}", typeof(List<T>));
+        }
+        static void Main()
+        {
+            PrintType<string>(); //使用string作为类型实参调用方法
+            PrintType<int>();    //使用int作为类型实参调用方法
+            Console.ReadKey();
+        }
+    }
+}
